3a7d6f859ca47ff39de18be8dfdbd444822549b2
Alessandro <alessandro.prataviera@ontariotechu.net>
2021-03-29 17:31:11 -0400

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+
+
+public class Controller {
+
+    @FXML
+    private Button upload;
+    @FXML
+    private ListView serverView;
+    @FXML
+    private Button download;
+    @FXML
+    private ListView clientView;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+}
